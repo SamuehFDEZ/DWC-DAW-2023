fd0b2ede74c4cacbd278a73c76de40f22253d7a3
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2023-11-09 15:56:24 +0100

    preparacion a mostrar las letras de la pelicula

--- Superahorcado/scripts/scripts.js
+++ Superahorcado/scripts/scripts.js
@@ -1,5 +1,15 @@
+let peliculaASolucionar = [];
+
+
 window.onload = function() {
     const teclado = document.getElementById("teclado");
+    const peliculas = ["El espíritu de la Navidad",
+        "Elf",
+        "The Polar Express",
+        "El origen de los guardianes",
+        "El Grinch"
+    ];
+
     for (let teclaActual = 65; teclaActual <= 90; teclaActual++) {
         const tecla = document.createElement("button");
         tecla.innerText = String.fromCharCode(teclaActual);
@@ -10,12 +20,6 @@ window.onload = function() {
         tecla.addEventListener("click", ultimaLetra)
         tecla.addEventListener("click", nombrePelicula);
 
-
-        function nombrePelicula() {
-
-
-
-        }
 
         function ultimaLetra() {
             const lastLetra = document.getElementById("ultimaLetra");
@@ -35,33 +39,43 @@ window.onload = function() {
             coleccionLetras.appendChild(nuevaLetra);
         }
     }
-    const peliculas = ["El espíritu de la Navidad",
-        "Elf",
-        "The Polar Express",
-        "El origen de los guardianes",
-        "El Grinch"
-    ];
 
-    const cabeza = document.getElementById("cabeza");
+
+
+    const aleatorios = peliculas[Math.floor(Math.random() * peliculas.length)].toUpperCase();
 
     const titulo = document.getElementById("titulo");
-    const aleatorios = peliculas[Math.floor(Math.random() * peliculas.length)];
     for (const aleatorio of aleatorios) {
         if (aleatorio === " ") {
             const espacio = document.createElement("span");
             espacio.innerHTML = "&nbsp; ";
             titulo.appendChild(espacio);
-        } else {
-            const linea = document.createElement("span");
+        }
+        else if(aleatorio !== " ") {
+            let linea = document.createElement("span");
             linea.innerHTML = "_ ";
             titulo.appendChild(linea);
-
+            linea.className = aleatorio;
+            //console.log(aleatorios.includes(this.innerText));
+            peliculaASolucionar.push(linea);
         }
     }
+
+    console.log(peliculaASolucionar);
+    function nombrePelicula() {
+        for (const aleatorio of aleatorios) {
+            if (aleatorios.includes(this.innerText)){
+                let linea = document.createElement("span");
+                linea.innerHTML = "_ ";
+                linea.innerHTML.replaceAll("_ ", this.innerText);
+            }
+        }
+
+        console.log(aleatorios.includes(this.innerText));
+
+    }
+
     console.log(aleatorios);
-    console.log(aleatorios.innerText)
-
-
 
 
     const extremidades = document.getElementsByClassName("extremidad");
@@ -69,4 +83,7 @@ window.onload = function() {
     for (const extremidad of extremidades) {
         extremidad.classList.add("papanoelEscondido");
     }
+
 }
+
+
