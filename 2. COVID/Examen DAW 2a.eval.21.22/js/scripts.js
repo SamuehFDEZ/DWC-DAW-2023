41606f224e86a59f7728a42e6e0ae46d4d4144e0
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2024-01-23 13:03:56 +0100

    siguen sin cargar las notas de los respectivos estados, json añadido simplemente por guia

--- 2. COVID/Examen DAW 2a.eval.21.22/js/scripts.js
+++ 2. COVID/Examen DAW 2a.eval.21.22/js/scripts.js
@@ -1,3 +1,18 @@
+let codigoEstados = [
+    "al", "ak", "az", "ar", "ca",
+    "co", "ct", "de", "fl", "ga",
+    "hi", "id", "il", "in", "ia",
+    "ks", "ky", "la", "me", "md",
+    "ma", "mi", "mn", "ms", "mo",
+    "mt", "ne", "nv", "nh", "nj",
+    "nm", "ny", "nc", "nd", "ok",
+    "or", "pa", "ri", "sc", "sd",
+    "tn", "tx", "ut", "vt", "va",
+    "wa", "wv", "wi", "wy"
+];
+
+let promesasFetch = [];
+
 window.onload = async () =>{
     await clickEstado();
 }
@@ -11,62 +26,33 @@ async function clickEstado(){
     }
 }
 
-
 //mostrar o no panel
 // funcion asyncrona para cargar el nombre del estado y sus notas
-async function mostrarPanel(cod){
+async function mostrarPanel() {
     modal.classList.toggle("oculto");
-    //let fips = area.data-cod;
-    let url = "https://api.covidtracking.com/v1/states/";
-    //let urlHispanos = `https://api.census.gov/data/2019/pep/charagegroups?get=POP&HISP=1&for=state:${2}`;
+
     titulo.innerText = this.title;
 
-    await fetch(url+cod+"info.json").then(data => data.json()).then(info => {
+    let url = `https://api.covidtracking.com/v1/states/info.json`;
+    await cargarNotas(url);
+
+
+
+}
+
+async function cargarNotas(url) {
+    await fetch(url).then(data => data.json()).then(info => {
         console.log(info);
         notas.innerText = info.notes;
-
     });
 }
 
-/*
-window.onload = async () => {
-    clickEstado();
-}
 
-function clickEstado() {
-    let area = document.getElementsByTagName("area");
-    for (const areaElement of area) {
-        areaElement.addEventListener("click", async () => {
-            let stateCode = areaElement.getAttribute("data-cod");
-            await cargarDatos(stateCode);
-            mostrarPanel();
-        });
-    }
 
-    modal.addEventListener("click", mostrarPanel);
-}
 
-function mostrarPanel() {
-    modal.classList.toggle("oculto");
-}
 
-async function cargarDatos(stateCode) {
-    let url = "https://api.covidtracking.com/v1/states/info.json";
 
-        await fetch(url).then(data => data.json()).then(info => {
-            if (stateCode){
-                document.getElementById("titulo").innerText = info.name;
 
-            }
-        });
 
-        // Find the state in the array based on the state code
-      /!*  let stateInfo = info.find(state => state.state === stateCode);
 
-        if (stateInfo) {
-        } else {
-            console.error("State information not found for code:", stateCode);
-        }*!/
 
-}
-*/
