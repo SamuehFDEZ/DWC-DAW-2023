45fda234b382d00d3d0b796277798b950f6964ba
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2024-01-26 13:08:43 +0100

    provincias empezado, problema con consolear el json

--- Provincias/js/main.js
+++ Provincias/js/main.js
@@ -1,1 +1,30 @@
-// https://api/explore/v2.1/catalog/datasets/provincias-espanolas/records?limit=52
+window.onload =  () =>{
+     cargarProvincias();
+}
+
+function cargarProvincias() {
+    let comunidad = document.querySelectorAll(".comunidad");
+
+    for (const com of comunidad) {
+        com.addEventListener("click", mostrarProvincias);
+    }
+}
+
+
+async function mostrarProvincias() {
+    let url = " https://api/explore/v2.1/catalog/datasets/provincias-espanolas/records?limit=52";
+
+    await fetch(url).then(data => data.json()).then(provincias => {
+        console.log(provincias);
+        //showProv(provincias);
+    });
+}
+
+
+/*function showProv(provincias) {
+    let table = document.createElement("table");
+    let td = document.createElement("td");
+
+    informacion.appendChild(table);
+    table.appendChild(td);
+}*/
