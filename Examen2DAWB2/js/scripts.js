a1d5caffbd4b087ef463ba19492a3462cd0b6ed0
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2024-02-01 16:39:06 +0100

    modificado red social

--- Examen2DAWB2/js/scripts.js
+++ Examen2DAWB2/js/scripts.js
@@ -49,8 +49,7 @@ window.onload = async () => {
 
     botonera = document.querySelector("#botonera");
 
-    usuarios = await cargarUsuarios();
-
+    await cargarUsuarios();
 
     cargarSelectUsuarios();
 }
@@ -59,35 +58,23 @@ window.onload = async () => {
 async function cargarUsuarios() {
     let url = "https://jsonplaceholder.typicode.com/users";
     await fetch(url).then(data => data.json()).then(info =>{
-
-        for (let i = 0; i < info.length; i++) {
-            let option = document.createElement("option");
-            option.value = info[i].id;
-            option.innerText = info[i].name;
-            document.getElementById("usuarios").appendChild(option);
-            nom = info[i].name.split(" ");
-            console.log(nom[0]);
-            //let le = "Leanne";
-        }
-        //calcularEdad(nom[0].age)
-        //cargarCiudad(info.lat, info.lng)
+       usuarios = info;
     });
 }
 
 //Función que devuelve el sexo del usuario
 async function estimarGenero(nombre) {
+    partes = nombre.split(" ");
+    nombre = partes[0];
     let url = `https://api.genderize.io?name=${nombre}`;
     await fetch(url).then(data => data.json()).then(info => {
-        return info.gender;
     });
 }
 //Función que devuelve la edad del usuario
 async function calcularEdad(nombre) {
     let url = `https://api.agify.io/?name=${nombre}`;
     await fetch(url).then(data => data.json()).then(info =>{
-        console.log(""+info);
 
-        //return info.name.age;
     });
 }
 
@@ -95,7 +82,7 @@ async function calcularEdad(nombre) {
 async function cargarCiudad(lat, lng) {
     let url = `https://geocode.xyz/${lat},${lng}?json=1`;
     await fetch(url).then(data => data.json()).then(info => {
-       console.log("cargarciudad"+info);
+
     });
 }
 
@@ -103,22 +90,103 @@ async function cargarCiudad(lat, lng) {
 //<option value=[id del usuario]>[nombre del usuario]</option>
 function cargarSelectUsuarios() {
     reiniciarParametros();
-    //A COMPLETAR
+    for (const usuario of usuarios) {
+        let option = document.createElement("option");
+        option.value = usuario.id;
+        option.innerText = usuario.name;
+        document.getElementById("usuarios").appendChild(option);
+    }
 }
 
 //Función genérica para la creación de elementos
-function crearElemento(atributos) {
-    //A COMPLETAR SI SE QUIERE
+function crearElemento(div, inner, clas, info) {
+    div.createElement("div");
+    inner.innerText;
 }
 
 
 //Filtrado de info utilizando array.filter u otro sistema
 async function mostrarDatosUsuario() {
+    let nombrePrimero = this.value-1;
+    document.getElementById("info").innerText = "";
     zonaPosts.innerHTML = "";
     zonaAlbums.innerHTML = "";
     zonaFotos.innerHTML = "";
-    //A COMPLETAR
+    console.log(this.value);
     botonera.classList.remove("oculto");
+    let fondo = document.createElement("div");
+    let foto = document.createElement("img");
+    fondo.id = "foto";
+    foto.src = "img/female.png";
+    fondo.appendChild(foto);
+    datosUsuarios.appendChild(fondo);
+
+    if (usuarios[nombrePrimero].gender === "female"){
+        foto.src = "img/female.png";
+
+    }
+    else if (usuarios[nombrePrimero].gender === "male"){
+        foto.src = "img/male.png";
+        fondo.appendChild(foto);
+        info.appendChild(fondo);
+    }
+
+    let divNombre = document.createElement("div");
+    divNombre.innerText = "Nombre:";
+    divNombre.classList.add("titulo");
+    document.getElementById("info").appendChild(divNombre);
+
+    let divDescripcioNombre = document.createElement("div");
+    divDescripcioNombre.innerText = usuarios[this.value-1].name;
+    divDescripcioNombre.classList.add("descripción");
+    document.getElementById("info").appendChild(divDescripcioNombre);
+
+    let divEdad = document.createElement("div");
+    divEdad.innerText = "Edad:";
+    divEdad.classList.add("titulo");
+    document.getElementById("info").appendChild(divEdad);
+
+    let divDescripcionEdad = document.createElement("div");
+    divDescripcionEdad.innerText = usuarios[this.value-1].age;
+    divDescripcionEdad.classList.add("descripción");
+    document.getElementById("info").appendChild(divDescripcionEdad);
+
+    let divEmail = document.createElement("div");
+    divEmail.innerText = "Email:";
+    divEmail.classList.add("titulo");
+    document.getElementById("info").appendChild(divEmail);
+
+    let divDescripcionEmail = document.createElement("div");
+    divDescripcionEmail.innerText = usuarios[this.value-1].email;
+    divDescripcionEmail.classList.add("descripción");
+    document.getElementById("info").appendChild(divDescripcionEmail);
+
+  /*  let aEnDescEmail = document.createElement("a");
+    aEnDescEmail.href = */
+
+    let divCiudad = document.createElement("div");
+    divCiudad.innerText = "Ciudad:";
+    divCiudad.classList.add("titulo");
+    document.getElementById("info").appendChild(divCiudad);
+
+    let divDescripcionCiudad = document.createElement("div");
+    divDescripcionCiudad.innerText = "Zeewolf Fracture Zone";
+    divDescripcionCiudad.classList.add("descripción");
+    document.getElementById("info").appendChild(divDescripcionCiudad);
+
+    let divWeb = document.createElement("div");
+    divWeb.innerText = "Web:";
+    divWeb.classList.add("titulo");
+    document.getElementById("info").appendChild(divWeb);
+
+    let divDescripcionWeb = document.createElement("div");
+    divDescripcionWeb.innerText = "hildegard.org";
+    divDescripcionWeb.classList.add("descripción");
+    document.getElementById("info").appendChild(divDescripcionWeb);
+
+
+
+    await estimarGenero(usuarios[nombrePrimero].name);
 }
 
 //Reiniciamos los parámetros para crear elementos.
