3fd113438092feacedc5ab9b40e1dab49fb43414
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2024-02-15 23:14:36 +0100

    spock esta terminadoo

--- piedraPapelTijeraLagartoSpok/js/scripts.js
+++ piedraPapelTijeraLagartoSpok/js/scripts.js
@@ -1,7 +1,6 @@
 let jugada = true;
 
 //Mensajes de los resultados de las jugadas
-
 var mensajes = {
     tipa: "Tijeras cortan papel",
     papi: "Papel tapa piedra",
@@ -15,7 +14,6 @@ var mensajes = {
     piti: "Piedra aplasta tijeras"
 }
 
-
 let imagenes = [
     "img/piedra.png",
     "img/papel.png",
@@ -26,11 +24,7 @@ let imagenes = [
 
 //Variables que contendrán los elementos HTML que vayamos a necesitar
 
-
 window.onload = () => {
-    cargarTablero();
-    asignarElementosHTML();
-    cargarEventos();
 
     let elementos = document.querySelectorAll(".contitem .item>img");
 
@@ -43,14 +37,6 @@ window.onload = () => {
     document.getElementById("seleccionado").addEventListener("dragover", allowDrop);
 }
 
-function asignarElementosHTML() {
-    //Función que utilizaremos para asignar los elementos HTML que vayamos a utilizar, a las varibales que hemos creado.
-}
-
-function cargarEventos() {
-    //Función donde cargaremos los eventos que necesite cada elemento de la partida
-}
-
 function continuar() {
     //Función que lanzamos cuando pulsamos al botón continuar
     //Volvemos a ocultar el mensaje;
@@ -60,11 +46,9 @@ function continuar() {
         let enemigo = document.querySelector("#enemigo img");
         let seleccionado = document.getElementById("seleccionado");
         document.getElementById("mensaje").className = "visible";
-        console.log(seleccionado.firstElementChild.dataset.puntos);
-        console.log(enemigo.id);
 
         mostrarResultado(seleccionado, enemigo, mensajes);
-        puntos();
+
         document.getElementById("proteccion").className = "visible";
         document.getElementById("deliveracion").className = "invisible";
         document.getElementById("continuar").addEventListener("click", () =>{
@@ -81,8 +65,6 @@ function continuar() {
 
     }
     enemigo.src = "img/interrogante.png";
-    
-    cargarTablero();
 }
 
 function imagenMaquina() {
@@ -97,121 +79,174 @@ function deliverar() {
     setTimeout(mostrarMensaje,3000);
 }
 
-function puntos() {
-    let divMio = document.createElement("div");
-    divMio.classList.add("punto");
-    divMio.classList.add("mio");
-    let divSuyo = document.createElement("div");
-    divSuyo.classList.add("punto");
-    divSuyo.classList.add("suyo");
-    document.getElementById("maquina").appendChild(divSuyo);
-
-   /* var mensajes = {
-        tipa: "Tijeras cortan papel",
-        papi: "Papel tapa piedra",
-        pila:"Piedra aplasta lagarto",
-        lasp: "Lagarto envenena a Spock",
-        spti: "Spock rompe tijeras",
-        tila: "Tijeras decapitan lagarto",
-        lapa: "Lagarto devora papel",
-        pasp: "Papel desautoriza a Spock",
-        sppi: "Spock vaporiza piedra",
-        piti: "Piedra aplasta tijeras"
-    }*/
-
-    if (parseInt(seleccionado.firstElementChild.dataset.puntos) === 1 &&
-        parseInt(enemigo.id) === 2) {
-        document.getElementById("maquina").appendChild(divSuyo);
-    }
-    else if (parseInt(seleccionado.firstElementChild.dataset.puntos) === 1 &&
-        parseInt(enemigo.id) === 3) {
-        document.getElementById("jugador").appendChild(divMio);
-    }
-    else if (parseInt(seleccionado.firstElementChild.dataset.puntos) === 1 &&
-        parseInt(enemigo.id) === 4) {
-        document.getElementById("jugador").appendChild(divMio);
-    }
-    else if (parseInt(seleccionado.firstElementChild.dataset.puntos) === 1 &&
-        parseInt(enemigo.id) === 5) {
-        document.getElementById("maquina").appendChild(divSuyo);
-    }
-   /* else if (parseInt(seleccionado.firstElementChild.dataset.puntos) === 2 &&
-        parseInt(enemigo.id) === 1) {
-        document.getElementById("jugador").appendChild(divMio);
-    }*/
-}
-
 function mostrarMensaje() {
     continuar();
-}
-
-function cargarTablero() {
-    //Función donde crearemos los elementos que vayamos a necesitar, junto a sus atributos y eventos
-    //La utilizaremos para reiniciar cada jugada
 }
 
 function mostrarResultado(seleccionado, enemigo, mensajes) {
     let puntosSeleccionado = parseInt(seleccionado.firstElementChild.dataset.puntos);
     let idEnemigo = parseInt(enemigo.id);
 
+    let divMio = document.createElement("div");
+    divMio.classList.add("punto");
+    divMio.classList.add("mio");
+    let divSuyo = document.createElement("div");
+    divSuyo.classList.add("punto");
+    divSuyo.classList.add("suyo");
+
     if (puntosSeleccionado === 1 && idEnemigo === 1) {
         document.querySelector("#mensaje p").innerHTML = "EMPATE";
+        enemigo.src = "img/interrogante.png";
+
     } else if (puntosSeleccionado === 1 && idEnemigo === 2) {
         document.querySelector("#mensaje p").innerHTML = mensajes.papi;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
+
     } else if (puntosSeleccionado === 1 && idEnemigo === 3) {
         document.querySelector("#mensaje p").innerHTML = mensajes.piti;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
+
     } else if (puntosSeleccionado === 1 && idEnemigo === 4) {
         document.querySelector("#mensaje p").innerHTML = mensajes.pila;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
     } else if (puntosSeleccionado === 1 && idEnemigo === 5) {
         document.querySelector("#mensaje p").innerHTML = mensajes.sppi;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
     }
+
+    /*----------------------------------------------------------------------------*/
 
     else if (puntosSeleccionado === 2 && idEnemigo === 1) {
         document.querySelector("#mensaje p").innerHTML = mensajes.papi;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
     } else if (puntosSeleccionado === 2 && idEnemigo === 2) {
         document.querySelector("#mensaje p").innerHTML = "EMPATE";
+        enemigo.src = "img/interrogante.png";
+
+
     } else if (puntosSeleccionado === 2 && idEnemigo === 3) {
         document.querySelector("#mensaje p").innerHTML = mensajes.tipa;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
     } else if (puntosSeleccionado === 2 && idEnemigo === 4) {
         document.querySelector("#mensaje p").innerHTML = mensajes.lapa;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
+
     } else if (puntosSeleccionado === 2 && idEnemigo === 5) {
         document.querySelector("#mensaje p").innerHTML = mensajes.pasp;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
     }
+
+    /*----------------------------------------------------------------------------*/
 
     else if (puntosSeleccionado === 3 && idEnemigo === 1) {
         document.querySelector("#mensaje p").innerHTML = mensajes.piti;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
     } else if (puntosSeleccionado === 3 && idEnemigo === 2) {
         document.querySelector("#mensaje p").innerHTML = mensajes.tipa;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
+
     } else if (puntosSeleccionado === 3 && idEnemigo === 3) {
         document.querySelector("#mensaje p").innerHTML = "EMPATE";
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
+
     } else if (puntosSeleccionado === 3 && idEnemigo === 4) {
         document.querySelector("#mensaje p").innerHTML = mensajes.tila;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
+
     } else if (puntosSeleccionado === 3 && idEnemigo === 5) {
         document.querySelector("#mensaje p").innerHTML = mensajes.spti;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
     }
+
+    /*----------------------------------------------------------------------------*/
 
     else if (puntosSeleccionado === 4 && idEnemigo === 1) {
         document.querySelector("#mensaje p").innerHTML = mensajes.pila;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
     } else if (puntosSeleccionado === 4 && idEnemigo === 2) {
         document.querySelector("#mensaje p").innerHTML = mensajes.lapa;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
+
     } else if (puntosSeleccionado === 4 && idEnemigo === 3) {
         document.querySelector("#mensaje p").innerHTML = mensajes.tila;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
+
     } else if (puntosSeleccionado === 4 && idEnemigo === 4) {
         document.querySelector("#mensaje p").innerHTML = "EMPATE";
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
     } else if (puntosSeleccionado === 4 && idEnemigo === 5) {
         document.querySelector("#mensaje p").innerHTML = mensajes.lasp;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
     }
+
+    /*----------------------------------------------------------------------------*/
 
     else if (puntosSeleccionado === 5 && idEnemigo === 1) {
         document.querySelector("#mensaje p").innerHTML = mensajes.sppi;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
     } else if (puntosSeleccionado === 5 && idEnemigo === 2) {
         document.querySelector("#mensaje p").innerHTML = mensajes.pasp;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
     } else if (puntosSeleccionado === 5 && idEnemigo === 3) {
         document.querySelector("#mensaje p").innerHTML = mensajes.spti;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("jugador").appendChild(divMio);
+
     } else if (puntosSeleccionado === 5 && idEnemigo === 4) {
         document.querySelector("#mensaje p").innerHTML = mensajes.lasp;
+        enemigo.src = "img/interrogante.png";
+        document.getElementById("maquina").appendChild(divSuyo);
+
     } else if (puntosSeleccionado === 5 && idEnemigo === 5) {
         document.querySelector("#mensaje p").innerHTML = "EMPATE";
+        enemigo.src = "img/interrogante.png";
+    }
+
+    let jugador = document.getElementById("jugador");
+    let maquina = document.getElementById("maquina");
+
+    let cuentaJugador = jugador.getElementsByTagName("div").length;
+    let cuentaMaquina = maquina.getElementsByTagName("div").length;
+
+    if (cuentaJugador === 10 || cuentaMaquina === 10){
+        location.reload();
     }
 }
 
@@ -222,7 +257,6 @@ function allowDrop(ev) {
 
 function drag(ev) {
     ev.dataTransfer.setData("id", this.id);
-    console.log(this);
 }
 
 function drop(ev) {
